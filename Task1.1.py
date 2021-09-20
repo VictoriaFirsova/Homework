c022aa9d0aff8f56608cabf04067d9d26d6f317b
VictoriaFirsova <62329437+VictoriaFirsova@users.noreply.github.com>
2021-09-20 14:58:37 +0300

    Delete Task1.1.py

--- Task1.1.py
+++
@@ -1,8 +0,0 @@
-# Task 1.1
-# Write a Python program to calculate the length of a string without using the `len` function.
-string = "измерить длину строки"
-count = 0
-
-for i in string:
-    count += 1
-
