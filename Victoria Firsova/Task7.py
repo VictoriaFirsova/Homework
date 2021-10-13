e0bac041f0d48850e47b1a6b7ec07ef5c9e9da3d
VictoriaFirsova <62329437+VictoriaFirsova@users.noreply.github.com>
2021-10-13 15:07:38 +0300

    Session 5 - Victoria Firsova
    Finished: Task 7

---
+++ Victoria Firsova/Task7.py
@@ -0,0 +1,6 @@
+"""Task 4.7.  Run the module modules/mod_a.py. Check its result. Explain why does this happen."""
+# we try to call variable from mod_c, then from mod_b, and it works
+"""Try to change x to a list [1,2,3]. Explain the result."""
+# we try to call variable from mod_c, then from mod_b, where it is still = 5. So we see 5 in the result
+"""Try to change import to from x import * where x - module names. Explain the result."""
+# I don't know. now a;; the logic for me was totally broken)
